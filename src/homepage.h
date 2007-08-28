a74267015585fa6e4ecd2b6d3c9f1acffc286845
Jean-Michel Frouin <jmfrouin@gnu.org>
2007-08-28 19:14:54 +0000

    Visual manual :D

--- src/homepage.h
+++ src/homepage.h
@@ -57,10 +57,7 @@ $Author$
  *
  * @section screenshots Screenshots
  *
- *  @image html gfx/scleaner0.2.0.png
- *
- *  @image html gfx/scleaner0.2.0.2.png
- *
+ *  @image html gfx/scleaner0.2.0.3.png
  *
  *
  *
