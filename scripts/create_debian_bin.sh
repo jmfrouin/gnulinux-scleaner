7345e2f5deed55d87c35d10e5507f81d6c558afe
Jean-Michel Frouin <jmfrouin@gnu.org>
2007-09-08 21:32:25 +0000

    Debianisation in progress

---
+++ scripts/create_debian_bin.sh
@@ -0,0 +1,22 @@
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+PROG=scleaner
+
+#!/bin/bash
+tar cvf ${PROG}_$1.tar scleaner --exclude=.svn --exclude=build > /dev/null
+echo Creating tar archive : ${PROG}_$1.tar
+gzip ${PROG}_$1.tar 
+echo Compression of tar archive : ${PROG}_$1.tar.gz
+sudo dpkg-buildpackage
+echo Build package
