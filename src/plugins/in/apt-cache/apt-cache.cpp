132581f952feed48aad157c797da0b94a54e980b
Jean-Michel Frouin <jmfrouin@gnu.org>
2007-10-14 11:43:38 +0200

    Adding apt cache plugin

---
+++ src/plugins/in/apt-cache/apt-cache.cpp
@@ -0,0 +1,73 @@
+/**
+ * This file is part of scleaner project.
+
+ * Copyright (C) 2007 FROUIN Jean-Michel
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <iostream>
+#include <sys/utsname.h>
+#include <plugins/inplugin_initializer.h>
+#include "apt-cache.h"
+#include <wx/dir.h>
+#include <leak/leak_detector.h>
+
+Plugins::CPluginInitializerIn<Capt_cachePlugin> g_apt_cache;
+
+Capt_cachePlugin::Capt_cachePlugin()
+{
+	setName("apt cache");
+}
+
+
+Capt_cachePlugin::~Capt_cachePlugin()
+{
+}
+
+
+Plugins::IPlugin::eType Capt_cachePlugin::Type()
+{
+	eType l_ret;
+	l_ret = eInput;
+	return l_ret;
+}
+
+
+void Capt_cachePlugin::getDirectory(std::string& _path)
+{
+	_path = "/var/cache/apt/archives/";
+}
+
+
+void Capt_cachePlugin::processFile(const std::string& _filename)
+{
+	if(	(_filename.find("abi", 0) != std::string::npos) || (_filename.find("config", 0) != std::string::npos) ||
+		(_filename.find("initrd", 0) != std::string::npos) || (_filename.find("System.map", 0) != std::string::npos) ||
+		(_filename.find("vmlinuz", 0) != std::string::npos))
+	{
+		m_fl.push_back(_filename);
+	}
+}
+
+bool Capt_cachePlugin::needRoot()
+{
+	bool l_ret = false;
+	l_ret = true;				 // /boot/ cannot be access by simple user
+	return l_ret;
+}
+
+
+/* vi:set ts=4: */
---
+++ src/plugins/in/apt-cache/apt-cache.h
@@ -0,0 +1,66 @@
+/**
+ * This file is part of scleaner project.
+
+ * Copyright (C) 2007 FROUIN Jean-Michel
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*! @page page3 Plugins documentations.
+ * - @subpage apt_cache
+ */
+
+/*! @page apt_cache apt_cache: input plugin.
+ * @section desc Description
+ * This plugin allow user to remove all <b>apt_cache</b> unused, automatically installed on system.
+ * <br>
+ * @section param Parameters
+ * @section pb Knows bugs
+ * @section todo Todo
+ */
+
+#ifndef _APT_CACHE_H_
+#define _APT_CACHE_H_
+
+#include <config.h>
+#include <plugins/root_plugin.h>
+
+/*!
+ *@brief Find unused apt_cache.
+ */
+class Capt_cachePlugin : public Plugins::IRootPlugin
+{
+	public:
+		Capt_cachePlugin();
+		~Capt_cachePlugin();
+
+		/*!
+		 *@brief From IPlugin.
+		 */
+		eType Type();
+
+		/*!
+		*@brief From Plugins::IRootPlugin
+		*/
+		void getDirectory(std::string& _path);
+
+		/*!
+		 *@brief From IInPlugin
+		 */
+		void processFile(const std::string& _filename);
+		bool needRoot();
+};
+#endif							 //_APT_CACHE_H_
+/* vi:set ts=4: */
