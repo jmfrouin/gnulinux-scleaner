013f64cceb05505efb9ca548f7baea7ae6ab065a
Jean-Michel Frouin <jmfrouin@gmail.com>
2008-04-03 23:19:34 +0200

    IThread header rewrote

--- src/tools/thread.h
+++ src/tools/thread.h
@@ -1,66 +1,72 @@
 /**
  * This file is part of scleaner project.
 
- * Copyright (C) 2007 FROUIN Jean-Michel
+ * Copyright (C) 2007,2008 FROUIN Jean-Michel
 
  * Visit scleaner website : http://www.scleaner.fr
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
-   
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-   
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _THREAD_H_
-#define _THREAD_H_
+#ifndef __THREAD_H__
+#define __THREAD_H__
 
 #include <pthread.h>
+#include <unistd.h>
+#include "mutex.h"
+typedef pthread_t Thread;
 
 namespace Tools
 {
-	/*!
-	 *@brief Thread another class by heritage.
-	 */
-	class IThread
-	{
-		public:
-			IThread();
-			virtual ~IThread();
-	
-			/*!
-			 *@brief Launch thread
-			 */
-			int Run();
-	
-			/*!
-			 *@brief The function to run to be pure virtual.
-			 */
-			virtual void __Run() = 0;
-	
-			/*!
-			 *@brief Need to be call when __Run has end.
-			 */
-			void __End();
-	
-		private:
-			pthread_t m_Thread;
-	
-	};
-	
-	static void* link(void* _context)
-	{
-		//IThread* l_Thread = dynamic_cast<IThread*>(_context);
-		IThread* l_Thread = (IThread*)_context;
-		l_Thread->__Run();
-		return 0;
-	}
+    /*!
+     *@brief Thread another class by heritage.
+     */
+    class IThread
+    {
+        public:
+            IThread();
+            virtual ~IThread();
+
+            /*!
+             *@brief Launch thread
+             */
+            int Start();
+
+            /*!
+             *@brief The function to run to be pure virtual.
+             */
+            virtual void Run() = 0;
+            const bool Running() const { return fRunning; }
+
+            static const int GetCount();
+            static const int GetMax();
+            static void SetMax(const int tmax);
+
+            /*!
+             *@brief Need to be call when Run has end.
+             */
+            void Stop();
+
+        private:
+            Thread fThread;
+            static int fCount;
+            static int fMax;
+            CMutex fLock;
+            bool fRunning;
+    };
+
+    int IThread::fCount = 0;
+    int IThread::fMax = 16;
 }
-#endif							 //_THREAD_H_
+#endif                           //__THREAD_H__
 /* vi:set ts=4: */
