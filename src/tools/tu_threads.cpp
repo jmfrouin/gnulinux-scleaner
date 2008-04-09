faca832fc95a99fec5e38a5d4e67eeba1b72d435
Jean-Michel Frouin <jmfrouin@gmail.com>
2008-04-09 13:28:50 +0200

    Unitary test for threads

---
+++ src/tools/tu_threads.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "thread.h"
+
+class A : public Tools::IThread
+{
+  public:
+    A() { }
+    ~A() { }
+    void Run()
+    {
+       for(int i=10; --i;)
+       {
+         std::cout << "A ";
+         sleep(1);
+       }
+    }
+};
+
+class B : public Tools::IThread
+{
+  public:
+    B() { }
+    ~B() { }
+    void Run()
+    {
+       while(fRunning)
+       {
+         std::cout << "B ";
+         sleep(1);
+       }
+    }
+};
+
+int main(int, char**)
+{
+  A CA;
+  B CB;
+  std::cout << "A started §\n";
+  std::cout << "B started !\n";
+  CA.Start();
+  CB.Start();
+  sleep(2);
+  CB.Stop();
+  std::cout << "\nB stoped \n";
+  std::cout << "Waiting end of threads ..." << '\n';
+  return EXIT_SUCCESS;
+}
