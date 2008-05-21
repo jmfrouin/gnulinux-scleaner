bbe4647f1638ad2290f9cd84fab1a0c6c0f95ab9
Jean Michel Frouin <jmfrouin@gmail.com>
2008-05-21 22:20:57 +0200

    wma input plugin

---
+++ src/plugins/in/wma/wma.cpp
@@ -0,0 +1,61 @@
+/**
+ * This file is part of scleaner project.
+
+ * Copyright (C) 2007, 2008 FROUIN Jean-Michel
+
+ * Visit scleaner website : http://www.scleaner.fr
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include <iostream>
+#include <plugins/inplugin_initializer.h>
+#include "wma.h"
+#include <sys/stat.h>            ///Get file size.
+#include <leak/leak_detector.h>
+#include <engine/engine.h>
+
+Plugins::CPluginInitializerIn<CwmaPlugin> gCoders;
+
+CwmaPlugin::CwmaPlugin()
+{
+    SetName("wma");
+}
+
+
+CwmaPlugin::~CwmaPlugin()
+{
+}
+
+
+void CwmaPlugin::ProcessFile(const std::string& filename)
+{
+    struct stat Info;
+    unsigned int FindWMA = filename.find(".wma", 0);
+    if(FindWMA == (filename.length()-4) )
+    {
+        std::string MP3File(filename.substr(0, FindWMA-1));
+        MP3File += ".mp3";
+        if(stat(MP3File.c_str(), &Info) == -1)
+           fFL.push_back(filename);
+    }
+}
+
+
+std::string CwmaPlugin::Description()
+{
+    return "Find music.wma if music.mp3 exist";
+}
+/* vi:set ts=4: */
---
+++ src/plugins/in/wma/wma.h
@@ -0,0 +1,65 @@
+/**
+ * This file is part of scleaner project.
+
+ * Copyright (C) 2007, 2008 FROUIN Jean-Michel
+
+ * Visit scleaner website : http://www.scleaner.fr
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*! @page page3 Plugins documentations.
+ * - @subpage wma
+ */
+
+/*! @page wma wma: input plugin.
+ * @section desc Description
+ * This plugin allow user to clean WMA files : <b>*.wma</b> nammed files.
+ * <br>
+ * @section param Parameters
+ * @section pb Knows bugs
+ * @todo Support both modes, simple user & root/sudo.
+ */
+
+#ifndef __WMA_H__
+#define __WMA_H__
+
+#include <config.h>
+#include <plugins/in_plugin.h>
+
+class CwmaPlugin : public Plugins::IInPlugin
+{
+    public:
+        CwmaPlugin();
+        ~CwmaPlugin();
+
+
+        bool GrabNullFile() { return false; }
+
+        bool GetDefaultSelection() { return true; }
+
+        /*!
+         *@brief From Plugins::IInPlugin
+         */
+        void ProcessFile(const std::string& filename);
+        void GetDirectory(std::string& path) { path = ""; }
+
+        /*!
+         *@brief From IPlugin
+         */
+        eType Type() { return eUserInput; }
+        std::string Description();
+};
+#endif                           //__WMA_H__
+/* vi:set ts=4: */
