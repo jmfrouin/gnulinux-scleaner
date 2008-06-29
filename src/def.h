ea99bbb2ddc3420e1d6f624099e9ab4b4454aa66
Jean Michel Frouin <jmfrouin@gmail.com>
2008-06-29 15:31:55 +0200

    Removing debug statements

--- src/def.h
+++ src/def.h
@@ -26,7 +26,7 @@
 #ifndef __DEF_H__
 #define __DEF_H__
 
-#define BUILD 1002
+#define BUILD 1003
 
 //For i8n
 #include <libintl.h>
--- src/update_build.sh
+++ src/update_build.sh
@@ -6,8 +6,6 @@
 # to avoid pb :)
 
 OLD=`awk '/#define BUILD.*/{print $3}' $1`
-echo ${OLD}
 NEW=`expr ${OLD} + 1`
-echo ${NEW}
 sed "s/#define BUILD ${OLD}/#define BUILD ${NEW}/" $1 > temp
 mv temp $1
